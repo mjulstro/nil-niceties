e9c1f72e37e8243c2d6b4b21b44a05a2dc63bd11
mjulstro <mjulstro@macalester.edu>
2018-04-29 15:10:04 -0500

    B)

--- ruby-counterpart/lib/profile_screen.rb
+++ ruby-counterpart/lib/profile_screen.rb
@@ -6,16 +6,16 @@ ProfileScreen = Model.new(:user, :app_theme) do
     # This line looks remarkably like Swift -- but doesn't work.
     # What is different? What needs to change?
 
-    #user.avatar&.style.background_color || app_theme.background_color
-    if (user.avatar != nil)
-    	if (user.avatar.style.background_color != nil)
-    		user.avatar.style.background_color
-    	else
-    		app_theme.background_color
-    	end
-    else
-    	app_theme.background_color
-    end
+    user.avatar&.style&.background_color || app_theme.background_color
+    # if (user.avatar != nil)
+    # 	if (user.avatar.style.background_color != nil)
+    # 		user.avatar.style.background_color
+    # 	else
+    # 		app_theme.background_color
+    # 	end
+    # else
+    # 	app_theme.background_color
+    # end
 
   end
 end
