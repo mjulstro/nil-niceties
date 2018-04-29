f2f891356274cb0293dfeb6a711071d404528b3e
mjulstro <mjulstro@macalester.edu>
2018-04-29 15:12:41 -0500

    Done B)

--- ruby-counterpart/lib/profile_screen.rb
+++ ruby-counterpart/lib/profile_screen.rb
@@ -7,15 +7,6 @@ ProfileScreen = Model.new(:user, :app_theme) do
     # What is different? What needs to change?
 
     user.avatar&.style&.background_color || app_theme.background_color
-    # if (user.avatar != nil)
-    # 	if (user.avatar.style.background_color != nil)
-    # 		user.avatar.style.background_color
-    # 	else
-    # 		app_theme.background_color
-    # 	end
-    # else
-    # 	app_theme.background_color
-    # end
 
   end
 end
